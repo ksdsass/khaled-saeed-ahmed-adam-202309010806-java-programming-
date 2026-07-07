488b1b50a71108cc60b4800cf65acc5a83cd7206
ksdsass <mrstrange4348@gmail.com>
2026-07-07 06:51:19 +0000

    OOP Week 7

--- totrieal /main.java
+++
@@ -1,8 +0,0 @@
-public class mean {
-    public static void main(String[] args) {
-        Student s1 = new student(studentName:"Khaled",studentAge:21, studentGpa:3.7);
-        s1.displayinfo();
-        s1.study();
-        s1.displayinfo();
-    }
-}
